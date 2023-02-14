7833966d621c02d47919cc9ea9835757a25202b4
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-02-14 18:41:42 +0700

    feat: using compression to decrease response size

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -10,9 +11,10 @@ async function bootstrap() {
     cors: true,
   });
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('app.port')!;
+  const port = configService.get<number>('app.port') || 3000;
 
   app.use(helmet());
+  app.use(compression());
   app.enableVersioning({ type: VersioningType.URI });
 
   await app.listen(port);
