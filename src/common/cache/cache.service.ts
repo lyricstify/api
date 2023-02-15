ef2a365f83d4d290e535384111cdedc13c0eef4c
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-02-15 17:12:52 +0700

    fix: prevent socket closed unexpectedly error

--- src/common/cache/cache.service.ts
+++ src/common/cache/cache.service.ts
@@ -4,19 +4,24 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { redisStore as store } from 'cache-manager-redis-yet';
+import { redisStore } from 'cache-manager-redis-yet';
+import type { RedisClientOptions } from 'redis';
 
 @Injectable()
 export class CacheService implements CacheOptionsFactory {
   constructor(private configService: ConfigService) {}
 
-  createCacheOptions(): CacheModuleOptions {
+  async createCacheOptions(): Promise<CacheModuleOptions<RedisClientOptions>> {
     const url = this.configService.get<string>('redis.url');
     const ttl = this.configService.get<number>('cache.ttl');
 
     return {
       ttl,
-      ...(url !== undefined && { store, url }),
+      ...(url !== undefined && {
+        store: redisStore,
+        url,
+        pingInterval: 4 * 60 * 1000,
+      }),
     };
   }
 }
