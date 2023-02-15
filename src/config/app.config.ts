0c4d301a7a63bcec92aaa8d84fd7b37f12e5a7e2
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-02-15 10:08:11 +0700

    fix: parse port to number

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,6 +3,6 @@ import ConfigEnvironment from './config.env';
 
 export default registerAs('app', () => ({
   env: process.env.APP_ENV || ConfigEnvironment.Development,
-  port: process.env.APP_PORT || 3000,
+  port: Number(process.env.APP_PORT) || 3000,
   spotifyCookie: process.env.SPOTIFY_COOKIE,
 }));
