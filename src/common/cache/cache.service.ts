02c15f6a9356ba79bf0e47898147659b2b0bdc73
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-02-16 18:32:20 +0700

    fix: prevent using redis while testing

--- src/common/cache/cache.service.ts
+++ src/common/cache/cache.service.ts
@@ -6,22 +6,26 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-yet';
 import type { RedisClientOptions } from 'redis';
+import ConfigEnvironment from '../config/config.env';
 
 @Injectable()
 export class CacheService implements CacheOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   async createCacheOptions(): Promise<CacheModuleOptions<RedisClientOptions>> {
+    const env = this.configService.get<`${ConfigEnvironment}`>('app.env');
     const url = this.configService.get<string>('redis.url');
     const ttl = this.configService.get<number>('cache.ttl');
 
     return {
       ttl,
-      ...(url !== undefined && {
-        store: redisStore,
-        url,
-        pingInterval: 4 * 60 * 1000,
-      }),
+      ...(url !== undefined && env !== 'testing'
+        ? {
+            store: redisStore,
+            url,
+            pingInterval: 4 * 60 * 1000,
+          }
+        : {}),
     };
   }
 }
