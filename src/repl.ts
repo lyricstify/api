1526073bc74e50ccfee80a7e9435ff6874324ca1
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-02-26 08:03:25 +0700

    fix(repl): add missing load testing env

--- src/repl.ts
+++ src/repl.ts
@@ -2,7 +2,7 @@ import { repl } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const replServer = await repl(AppModule.register());
+  const replServer = await repl(AppModule.register('.env.testing'));
   replServer.setupHistory('.nestjs_repl_history', (err) => {
     if (err) {
       console.error(err);
