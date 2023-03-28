ffac59573d866f6698e6a6dea3bf1509b9e39427
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-28 12:25:59 +0700

    fix(http): use not found status code by default

--- src/common/http/http.catch-axios-error.ts
+++ src/common/http/http.catch-axios-error.ts
@@ -1,20 +1,14 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { AxiosError } from 'axios';
 import { catchError } from 'rxjs';
 
 export const httpCatchAxiosError = <T>({
   defaultStatusText,
-  defaultStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
+  defaultStatusCode = HttpStatus.NOT_FOUND,
 }: {
   defaultStatusText: string;
   defaultStatusCode?: number;
 }) => {
-  return catchError<T, never>((error: AxiosError) => {
-    const response: [string, number] =
-      error.response !== undefined
-        ? [error.response.statusText, error.response.status]
-        : [defaultStatusText, defaultStatusCode];
-
-    throw new HttpException(...response);
+  return catchError<T, never>(() => {
+    throw new HttpException(defaultStatusText, defaultStatusCode);
   });
 };
