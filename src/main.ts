c380cf1a4ee0256f250c8027e7b73accd097f58e
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-29 11:39:28 +0700

    fix(main): add export main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule.register(),
     {
