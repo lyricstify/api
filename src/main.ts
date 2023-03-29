a75ccabdb87bc2947b0e87bbb92780762a3394e6
yt2951 <rahman.yusuftaufiq@gmail.com>
2023-03-29 11:22:26 +0700

    fix(main): revert to original main.ts without swagger

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,8 @@ import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import {
-  DocumentBuilder,
-  SwaggerDocumentOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
-import ConfigEnvironment from './common/config/config.env';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -20,35 +14,9 @@ async function bootstrap() {
   );
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port') || 3000;
-  const env = configService.get<`${ConfigEnvironment}`>('app.env');
 
   app.use(helmet());
   app.enableVersioning({ type: VersioningType.URI });
-
-  if (env === 'development') {
-    const config = new DocumentBuilder()
-      .setTitle('Lyricstify API')
-      .setDescription('Discover time-synced Spotify song lyrics.')
-      .setExternalDoc('GitHub Repository', 'https://github.com/lyricstify/api')
-      .setVersion('1.0')
-      .addServer(
-        'https://api.lyricstify.vercel.app/',
-        'Production server Lyricstify API.',
-      )
-      .addServer(
-        `http://localhost:${port}`,
-        'Local server Lyricstify API for development.',
-      )
-      .addTag('lyrics')
-      .build();
-
-    const options: SwaggerDocumentOptions = {
-      operationIdFactory: (controllerKey, methodKey) => methodKey,
-    };
-    const document = SwaggerModule.createDocument(app, config, options);
-
-    SwaggerModule.setup('docs', app, document);
-  }
 
   await app.listen(port);
 }
